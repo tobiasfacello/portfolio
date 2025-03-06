914b6608bc8ccdb1023ccb9bd8e33cfd240d9b9d
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-03-06 19:53:09 -0300

    Updates Project Card component's index.tsx file (adds onClick event handler; adds new format support)

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,59 +1,47 @@
-import { useState } from "react";
-import { StyleSheetManager } from "styled-components";
-import { StyledProjectCard } from "./styled";
+import { useState } from 'react';
+import { StyleSheetManager } from 'styled-components';
+import { StyledProjectCard } from './styled';
 
 //* Components
-import Text from "../Text";
-import Container from "../Containers/Container";
+import Text from '../Text';
+import Container from '../Containers/Container';
 
 function ProjectCard(props: any) {
 	const [isHovered, setIsHovered] = useState(false);
 
 	const filteredProps: string[] = [
-		"isHovered",
-		"p",
-		"m",
-		"variant",
-		"title",
-		"details",
-		"src",
+		'isHovered',
+		'variant',
+		'title',
+		'details',
+		'src',
+		'url',
 	];
+
+	const handleClick = (url: string) => {
+		window.location.href = url;
+	};
 
 	return (
 		<StyleSheetManager
 			shouldForwardProp={(prop) => !filteredProps.includes(prop)}
 		>
 			<StyledProjectCard
+				onClick={() => handleClick(props.url)}
 				onMouseEnter={() => setIsHovered(true)}
 				onMouseLeave={() => setIsHovered(false)}
 				isHovered={isHovered}
-				p={props.p}
-				m={props.m}
 			>
-				<Container
-					direction={"column"}
-					justify={"flex-start"}
-					align={"start"}
-				>
-					<Text
-						variant={"subtitle-fst"}
-						m={["xxxs", "xs", "xs", "xs"]}
-					>
+				<Container direction={'column'} justify={'flex-start'} align={'start'}>
+					<Text variant={'subtitle-fst'} m={['xxxs', 'xs', 'xs', 'xs']}>
 						{props.title}
 					</Text>
-					<Text
-						variant={"details-fst"}
-						m={["zero", "zero", "xs", "xs"]}
-					>
+					<Text variant={'details-fst'} m={['zero', 'zero', 'xs', 'xs']}>
 						{props.details}
 					</Text>
 				</Container>
-				<Container
-					direction={"column"}
-					justify={"center"}
-					align={"end"}
-				>
-					<img src={props.src}></img>
+				<Container direction={'column'} justify={'center'} align={'end'}>
+					<img src={props.src} />
 				</Container>
 			</StyledProjectCard>
 		</StyleSheetManager>

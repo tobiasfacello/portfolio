33a1ba3b7f5d5f9bab57cd1db7525d9a5f0f1c43
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-04 18:06:03 -0300

    Updates Home.tsx (Page) file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,60 +16,66 @@ function Home() {
 			<MediaQuery minWidth={360} maxWidth={959}>
 				<Container
 					w={'100%'}
-					h={'calc(100vh - 64px)'}
+					h={'100%'}
 					direction={'column'}
 				>
-					<Header></Header>
-					<Profile></Profile>
-					<About></About>
+					<Header />
+					<Profile />
+					<About />
 				</Container>
-				<Projects></Projects>
-				<Skills></Skills>
-				<Works></Works>
-				<Footer></Footer>
+				<Projects />
+				<Skills />
+				<Works />
+				<Footer />
 			</MediaQuery>
 
 			{/* Tablet Layout */}
 			<MediaQuery minWidth={960} maxWidth={1279}>
 				<Container
 					w={'100%'}
-					h={'calc(100vh - 64px)'}
+					h={'100%'}
 					direction={'column'}
 				>
-					<Header></Header>
-					<Profile></Profile>
-					<About></About>
+					<Header />
+					<Profile />
+					<About />
 				</Container>
-				<Projects></Projects>
-				<Skills></Skills>
-				<Works></Works>
-				<Footer></Footer>
+				<Projects />
+				<Skills />
+				<Works />
+				<Footer />
 			</MediaQuery>
 
 			{/* Desktop Layout */}
 			<MediaQuery minWidth={1280} maxWidth={1439}>
 				<Container
 					w={'100%'}
-					h={'calc(90vh - 64px)'}
+					h={'100%'}
 					direction={'column'}
 					justify={'center'}
 					align={'center'}
 				>
-					<Header></Header>
+					<Header />
 					<Container
 						w={'100%'}
+						h={'100%'}
 						justify={'center'}
 						align={'center'}
 						style={'border-top: 1px solid var(--secondary-60);'}
 					>
 						<Container
 							w={'90%'}
+							h={"100%"}
 							justify={'center'}
 							align={'center'}
+							style={`
+								border-left: 1px solid var(--secondary-60);
+								border-right: 1px solid var(--secondary-60);
+								`}
 						>
-							<About></About>
-							<Profile></Profile>
-							<Projects></Projects>
+							<About />
+							<Profile />
+							<Projects />
 						</Container>
 					</Container>
 				</Container>
@@ -82,21 +88,21 @@ function Home() {
 					style={'border-top: 1px solid var(--secondary-60);'}
 				>
 					<Container w={'90%'} justify={'center'} align={'center'}>
-						<Skills></Skills>
-						<Works></Works>
+						<Skills />
+						<Works />
 					</Container>
 				</Container>
-				<Footer></Footer>
+				<Footer />
 			</MediaQuery>
 			<MediaQuery minWidth={1440} maxWidth={1800}>
 				<Container
 					w={'100%'}
-					h={'calc(90vh - 64px)'}
+					h={'100%'}
 					direction={'column'}
 					justify={'center'}
 					align={'center'}
 				>
-					<Header></Header>
+					<Header />
 					<Container
 						w={'100%'}
 						justify={'center'}
@@ -105,12 +111,19 @@ function Home() {
 					>
 						<Container
 							w={'90%'}
+							h={"100%"}
+							minH={"585px"}
+							maxH={"585px"}
 							justify={'center'}
 							align={'center'}
+							style={`
+								border-left: 1px solid var(--secondary-60);
+								border-right: 1px solid var(--secondary-60);
+								`}
 						>
-							<About></About>
-							<Profile></Profile>
-							<Projects></Projects>
+							<About />
+							<Profile />
+							<Projects />
 						</Container>
 					</Container>
 				</Container>
@@ -122,35 +135,40 @@ function Home() {
 					style={'border-top: 1px solid var(--secondary-60);'}
 				>
 					<Container w={'90%'} justify={'center'} align={'center'}>
-						<Skills></Skills>
-						<Works></Works>
+						<Skills />
+						<Works />
 					</Container>
 				</Container>
-				<Footer></Footer>
+				<Footer />
 			</MediaQuery>
 			<MediaQuery minWidth={1801}>
 				<Container
 					w={'100%'}
-					h={'calc(80vh - 64px)'}
 					direction={'column'}
 					justify={'center'}
 					align={'center'}
 				>
-					<Header></Header>
+					<Header />
 					<Container
 						w={'100%'}
+						h={'100%'}
 						justify={'center'}
 						align={'center'}
 						style={'border-top: 1px solid var(--secondary-60);'}
 					>
 						<Container
 							w={'90%'}
+							h={"fit-content"}
 							justify={'center'}
 							align={'center'}
+							style={`
+								border-left: 1px solid var(--secondary-60);
+								border-right: 1px solid var(--secondary-60);
+								`}
 						>
-							<About></About>
-							<Profile></Profile>
-							<Projects></Projects>
+							<About />
+							<Profile />
+							<Projects />
 						</Container>
 					</Container>
 				</Container>
@@ -163,11 +181,11 @@ function Home() {
 					style={'border-top: 1px solid var(--secondary-60);'}
 				>
 					<Container w={'90%'} justify={'center'} align={'center'}>
-						<Skills></Skills>
-						<Works></Works>
+						<Skills />
+						<Works />
 					</Container>
 				</Container>
-				<Footer></Footer>
+				<Footer />
 			</MediaQuery>
 		</>
 	);

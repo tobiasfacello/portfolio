bf186a2df565e6e435d54b694a0cec3058a8403c
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-04 18:03:17 -0300

    Updates Project Card component (styled.tsx and index.tsx file)

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -5,6 +5,7 @@ import { StyledProjectCard } from './styled';
 //* Components
 import Text from '../Text';
 import Container from '../Containers/Container';
+import PillTag from '../Pill';
 
 function ProjectCard(props: any) {
 	const [isHovered, setIsHovered] = useState(false);
@@ -14,6 +15,7 @@ function ProjectCard(props: any) {
 		'variant',
 		'title',
 		'details',
+		'tag',
 		'src',
 		'url',
 	];
@@ -32,13 +34,18 @@ function ProjectCard(props: any) {
 				onMouseLeave={() => setIsHovered(false)}
 				isHovered={isHovered}
 			>
-				<Container direction={'column'} justify={'flex-start'} align={'start'}>
-					<Text variant={'subtitle-fst'} m={['xxxs', 'xs', 'xs', 'xs']}>
+				<Container direction={'column'} justify={'space-between'} align={'start'}>
+					<Text variant={'subtitle-fst'}>
 						{props.title}
 					</Text>
-					<Text variant={'details-fst'} m={['zero', 'zero', 'xs', 'xs']}>
+					<Text variant={'details-fst'}>
 						{props.details}
 					</Text>
+					<PillTag
+						tag={props.tag}
+						maxW={'140px'}
+						p={['4', '8', '4', '8']}
+					/>
 				</Container>
 				<Container direction={'column'} justify={'center'} align={'end'}>
 					<img src={props.src} />
--- src/components/ProjectCard/styled.tsx
+++ src/components/ProjectCard/styled.tsx
@@ -1,22 +1,25 @@
 import styled from 'styled-components';
 
+//* Components
+import { StyledPillTag } from '../Pill/styled';
+
 export const StyledProjectCard = styled.div<{
 	isHovered: boolean;
 }>`
-	width: 320px;
+	width: 100%;
+	max-width: 335px;
 	height: 123px;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	padding: 5px;
-	margin-bottom: 20px;
+	padding: 10px;
 	background-color: rgba(197, 199, 188, 5%);
 	background-clip: padding-box;
 	border: 1px solid transparent;
 	/* box-shadow: 0px -1px 0px 0px rgba(255, 255, 255, 0.2),
 		-1px 0px 0px 0px rgba(255, 255, 255, 0.1),
 		1px 0px 0px 0px rgba(255, 255, 255, 0.1); */
-	border-radius: 10px;
+	border-radius: 20px;
 	backdrop-filter: blur(4px);
 	cursor: pointer;
 	transition: all 0.3s;
@@ -25,7 +28,7 @@ export const StyledProjectCard = styled.div<{
 		content: '';
 		position: absolute;
 		inset: 0;
-		border-radius: 10px;
+		border-radius: 20px;
 		padding: 1px;
 		background: linear-gradient(
 			to bottom,
@@ -50,24 +53,20 @@ export const StyledProjectCard = styled.div<{
 
 	@media (min-width: 768px) and (max-width: 1279px) {
 		& {
-			width: 380px;
-		}
-	}
-
-	@media (min-width: 1280px) and (max-width: 1339px) {
-		& {
-			margin-bottom: 8px;
-		}
-	}
-
-	@media (min-width: 1440px) {
-		& {
-			width: 380px;
+			max-width: 380px;
 		}
 	}
 
 	& img {
-		opacity: ${(props) => (props.isHovered ? '1' : '0.2')};
+		opacity: ${(props) => (props.isHovered ? '1' : '0.6')};
 		transition: all 0.3s;
+	}
+
+	${StyledPillTag} {
+		color: ${(props) =>
+		props.isHovered ? "var(--pill-text-hovered)" : "var(--text)"};
+		background-color: ${(props) =>
+		props.isHovered ? "var(--text)" : "transparent"};
+		opacity: ${(props) => (props.isHovered ? "1" : "0.6")};
 	}
 `;

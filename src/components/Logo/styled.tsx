e5de3aaeccf2a6deb745ebdf82adf04e5300ae23
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-04-06 23:54:39 -0300

    Updates Logo component (styled.tsx file)

--- src/components/Logo/styled.tsx
+++ src/components/Logo/styled.tsx
@@ -13,13 +13,16 @@ export const StyledLogo = styled.h1`
 
 	@media (min-width: 360px) {
 		font-size: 78px;
+		line-height: 78px;
 	}
-
+	
 	@media (min-width: 425px) {
 		font-size: 92px;
+		line-height: 92px;
 	}
-
+	
 	@media (min-width: 1440px) {
 		font-size: 140px;
+		line-height: 140px;
 	}
 `;

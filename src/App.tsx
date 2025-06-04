5ba6c0f25b76671d01408bf2662e42af29703207
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-06-04 18:59:07 -0300

    Updates root App (removes Media Query component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import ReactLenis from "lenis/react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Cursor from "./components/Cursor";
-import MediaQuery from "react-responsive";
 import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
 	return (
 		<>
-			<MediaQuery minWidth={360} maxWidth={767}>
-				<ReactLenis options={{ lerp: 0.1, duration: 1.7, smoothWheel: true }} root>
-					<ThemeProvider>
-						<Home />
-					</ThemeProvider >
-				</ReactLenis>
-			</MediaQuery>
-			<MediaQuery minWidth={768}>
-				<ReactLenis options={{ lerp: 0.1, duration: 1.7, smoothWheel: true }} root>
-					<ThemeProvider>
-						<Cursor />
-						<Home />
-					</ThemeProvider >
-				</ReactLenis>
-			</MediaQuery>
+			<ReactLenis options={{ lerp: 0.1, duration: 1.7, smoothWheel: true }} root>
+				<ThemeProvider>
+					<Cursor />
+					<Home />
+				</ThemeProvider >
+			</ReactLenis>
 		</>
 	);
 }

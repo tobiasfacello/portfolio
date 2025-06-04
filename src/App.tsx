248a47e28f2f1a9bd6d00a0e865db2c382918cb5
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-06-04 17:33:51 -0300

    Updates root App (adds Media Query component to fix Cursor appearance issue)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import ReactLenis from "lenis/react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Cursor from "./components/Cursor";
+import MediaQuery from "react-responsive";
 import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
 	return (
 		<>
-			<ReactLenis options={{ lerp: 0.1, duration: 1.7, smoothWheel: true }} root>
-				<ThemeProvider>
-					<Cursor />
-					<Home />
-				</ThemeProvider >
-			</ReactLenis>
+			<MediaQuery minWidth={360} maxWidth={767}>
+				<ReactLenis options={{ lerp: 0.1, duration: 1.7, smoothWheel: true }} root>
+					<ThemeProvider>
+						<Home />
+					</ThemeProvider >
+				</ReactLenis>
+			</MediaQuery>
+			<MediaQuery minWidth={768}>
+				<ReactLenis options={{ lerp: 0.1, duration: 1.7, smoothWheel: true }} root>
+					<ThemeProvider>
+						<Cursor />
+						<Home />
+					</ThemeProvider >
+				</ReactLenis>
+			</MediaQuery>
 		</>
 	);
 }
